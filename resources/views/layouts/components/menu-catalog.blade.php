5016a0fcaefa92b52e1d77a790b224b0a944168b
AntonBs <reloded1995@gmail.com>
2022-03-31 18:26:44 +0300

    Menu before refactoring

--- resources/views/layouts/components/menu-catalog.blade.php
+++ resources/views/layouts/components/menu-catalog.blade.php
@@ -1,27 +1,31 @@
 <div class="menu_catalog">
     @foreach($catalog as $catalog_item)
-        <div class="mc_link">
-            <a href="{{route( 'category',['slug'=>$catalog_item->slug])}}">
-                <div class="text_catalog">
-                    <div class="icon icon_mobile"></div>
-                    {{$catalog_item->name}}
+    <div class="mc_link">
+        <a href="{{route( 'category',['slug'=>$catalog_item->slug])}}">
+            <div class="text_catalog">
+                <div class="icon">
+                    <img src="{{asset('assets/uploads/menu/')}}/{{$catalog_item->icon}}" alt="img">
                 </div>
-                <span class="icon icon_arr_right"></span>
-            </a>
+                {{$catalog_item->name}}
+            </div>
+            <span class="icon icon_arr_right"></span>
+        </a>
+        <div class="wrap_un_list">
             <div class="un_list">
+                @foreach($catalog_item->children as $item)
                 <div class="wrap_list_un_menu">
-                    @foreach($catalog_item->children as $item)
-                        <h3 class="title_un_menu">
-                            <a href="{{route('category',['slug'=>$item->slug])}}">{{$item->name}}</a>
-                        </h3>
-                        @foreach($item->children as $child)
-                            <div class="un_menu">
-                                <a href="{{route('category',['slug'=>$child->slug])}}">{{$child->name}}</a>
-                            </div>
-                        @endforeach
+                    <h3 class="title_un_menu">
+                        <a href="{{route('category',['slug'=>$item->slug])}}">{{$item->name}}</a>
+                    </h3>
+                    @foreach($item->children as $child)
+                    <div class="un_menu">
+                        <a href="{{route('category',['slug'=>$child->slug])}}">{{$child->name}}</a>
+                    </div>
                     @endforeach
                 </div>
+                @endforeach
             </div>
         </div>
+    </div>
     @endforeach
 </div>
